18e206ae5c1b0a9f8f26e418f4fbce0dd75167c0
Prriyam <Prriyam@users.noreply.github.com>
2026-05-17 04:13:36 +0000

    deploy: 5128bb7e3872ac77b61c1af34f84166d3d95fbb8

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {};
+const EMAIL_CONFIG = {
+    EMAILJS_PUBLIC_KEY:  "QtZ2e4ENDLjrfCsx_",
+    EMAILJS_SERVICE_ID:  "service_ql1at5w",
+    EMAILJS_TEMPLATE_ID: "template_enzo6h5"
+};
